d476377f048323082d49505c6c0067b6044bb8c3
Andrea Freeman <afreeman5566@gmail.com>
2021-11-15 20:22:39 -0600

    created basic CRUD controllers for bills

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,58 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+ # Bill Routers---------------------------------------------
+ #We can probably break these into different controllers
+ 
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "Howdy I'm working! 🚀🚀🚀 Please don't break me " }.to_json
   end
 
+  #gets all bills
+  get "/bill" do
+    bills = Bill.all
+    bills.to_json
+  end
+
+  #gets a specific bill
+
+  get "/bill/:id" do
+    a_bill = Bill.find(params[:id])
+    a_bill.to_json
+
 end
+
+#deletes a specific bill
+
+delete "/bill/:id" do
+  a_bill = Bill.find(params[:id])
+  a_bill.destroy
+  a_bill.to_json
+end
+
+#creates a new bill
+
+post "/bill/:id" do
+    make_bill = Bill.create(
+    bill_name: params[:bill_name],
+    bill_amount: params[:bill_amount],
+    category_name: params[:category_name]
+    )
+    make_bill.to_json
+end
+
+# edits a specific bill
+
+patch "/bill/:id" do
+    a_bill = Bill.find(params[:id])
+    bill.update(
+      bill_name: params[:bill_name],
+      bill_amount: params[:bill_amount],
+      category_name: params[:category_name]
+    )
+    a_bill.to_json
+end
+
+
+end
+#--------------------------------------------------------------------
