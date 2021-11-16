340170042bf9eac2cdc27800dd6033c9b58fe1ab
Andrea Freeman <afreeman5566@gmail.com>
2021-11-16 14:10:10 -0600

    added more content to bills, updated controllers and added amount to wallet

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,10 +1,4 @@
 class Wallet < ActiveRecord::Base
-<<<<<<< HEAD
-    belongs_to :user 
-    has_many :bills 
-end  
-=======
      belongs_to :user   
      has_many :bills
 end
->>>>>>> 530f4592e8ef7833cbe5b51c420d7011f9e58a0f
